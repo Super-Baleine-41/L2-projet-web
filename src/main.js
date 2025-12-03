23db89ec6d2038d19ffa36ef8388307d3355f48d
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-12-03 19:16:42 +0100

    feat(views): Add state-driven history for improved navigation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './style.css';
+import BaseView from "@ui/BaseView.js";
 import AppState from "@utils/AppState.js";
+import PokeAPI from "@utils/PokeAPI.js";
 import MainMenuView from "@views/MainMenuView.js";
+import views from "@views";
 
 const app = document.querySelector('#app');
 
@@ -8,7 +11,24 @@ const app = document.querySelector('#app');
 	// Initialize global application state
 	const appState = new AppState();
 	const api = new PokeAPI();
+	window.addEventListener("popstate", async (event) => {
+		const state = event.state;
 
+		if (!state) {
+			await BaseView.switchView(MainMenuView, app, appState, api, false);
+			return;
+		}
+
+		const { view, args } = state;
+		const ViewClass = views[view];
+
+		if (!ViewClass) {
+			await BaseView.switchView(MainMenuView, app, appState, api, false);
+			return;
+		}
+
+		await BaseView.switchView(ViewClass, app, appState, api, false, ...(args || []));
+	});
 	// Create and render the main menu
 	const mainMenu = new MainMenuView(app, appState, api);
 	appState.setCurrentView(mainMenu);
---
+++ src/views/index.js
@@ -0,0 +1,13 @@
+import BattleView from "@views/BattleView.js";
+import MainMenuView from "@views/MainMenuView.js";
+import PokemonListView from "@views/PokemonListView.js";
+
+
+const views = {
+	BattleView,
+	MainMenuView,
+	PokemonListView
+};
+
+export default views;
+export { BattleView, MainMenuView, PokemonListView };
